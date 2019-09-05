37e457d228beec8a161b499c49ab715ecc070d1a
ctsjava <ctsgit@gmail.com>
2019-09-05 10:42:16 +0530

    day4

---
+++ Java SE/basic-preograms/Sample.java
@@ -0,0 +1,19 @@
+class Sample{
+    public static void main(String args[]){
+        // If 
+
+       java.util.Scanner sc=new java.util.Scanner(System.in);
+       System.out.println("Input a number: ");
+       int a=sc.nextInt();
+    
+          // 10%2 = 0
+          // 10/2 = 5
+
+        if(a%2 == 0){  
+            System.out.println("Even");
+        }else{
+            System.out.println("Odd Number");
+        }
+
+    }
+}
