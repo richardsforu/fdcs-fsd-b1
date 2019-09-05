cfdf381cfadbbe9b305546a237e4a922518ac06d
ctsjava <ctsgit@gmail.com>
2019-09-05 12:09:52 +0530

    day4

---
+++ Java SE/basic-preograms/ForLoopDemo.java
@@ -0,0 +1,21 @@
+class ForLoopDemo{
+
+    
+    public static void main(String args[]){
+
+     // break and continue
+
+      for(int i=1;i<=10;i++){
+        if(i==4){
+          break;
+        }
+        System.out.println(i);
+      }
+
+      
+
+        
+       
+
+    }
+}
---
+++ Java SE/basic-preograms/MenuDrivenDemo.java
@@ -0,0 +1,50 @@
+class MenuDrivenDemo{    
+    public static void main(String args[]){
+
+   java.util.Scanner sc=new java.util.Scanner(System.in);
+     while(true){
+         System.out.println("Menu");
+         System.out.println("-------------");
+         System.out.println("1.Save");
+         System.out.println("2.List All");
+         System.out.println("3.Find");
+         System.out.println("4.Delete");
+         System.out.println("5.Update");
+         System.out.println("6.Exit");
+         System.out.println("Enter your choice: ");
+         int ch=sc.nextInt();
+
+         switch(ch){
+             case 1:
+             System.out.println("Saving data");
+             break;
+             case 2:
+             System.out.println("Listing all data");
+             break;
+             case 3:
+             System.out.println("Finding one record");
+             break;
+             case 4:
+             System.out.println("Deleting record");
+             break;
+             case 5:
+             System.out.println("Updating record");
+             break;
+             case 6:
+             System.out.println("Thank you. Visit again");
+             System.exit(1);
+             default:
+             System.out.println("Invalid Choice. try again: ");
+
+         }
+
+
+
+     }
+      
+
+        
+       
+
+    }
+}
