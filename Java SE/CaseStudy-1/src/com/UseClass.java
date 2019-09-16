4a1772ef60547f8942b3dc8a7919120ca77f750e
richardsforu <praveen.somireddy@gmail.com>
2019-09-16 12:09:32 +0530

    Inheritance

--- Java SE/CaseStudy-1/src/com/UseClass.java
+++ Java SE/CaseStudy-1/src/com/UseClass.java
@@ -5,6 +5,8 @@ public class UseClass {
 	public static void main(String[] args) {
 		Student s1 = new Student("Abc", "Married", 10, 'M', "10/7/1987", "HYDERABAD", "abc@abc.com", "xyz@xyz.com",
 				9876543765L, "Java", "Mtech", "Indian");
+		
+		
 		s1.registerStudent();
 
 		if (s1.getAdmissionId() != null) {
