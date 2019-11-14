b1c0ae8802eb37c5a88cf9292b98cf602908f137
praveen.somireddy@gmail.com <praveen.somireddy@gmail.com>
2019-11-14 10:48:11 +0530

    Java Script

--- UI/JS/GUI/data2.js
+++ UI/JS/GUI/data2.js
@@ -1,3 +1,8 @@
 function loadImage(){
     document.getElementById("d1").innerHTML="<img src='images/img1.png' height='150' width='150'>"
 }
+
+function loadImage1(imagePath){
+    var imageUrl="<img src=images/"+imagePath+" height='200' width='200'>";
+    document.getElementById("d2").innerHTML=imageUrl;
+}
