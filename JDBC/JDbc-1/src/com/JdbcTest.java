86c5b9e975ce3f30c867fae51e2535e5d71f4cf0
praveen.somireddy@gmail.com <praveen.somireddy@gmail.com>
2019-11-18 12:33:59 +0530

    JDBC

--- JDBC/JDbc-1/src/com/JdbcTest.java
+++ JDBC/JDbc-1/src/com/JdbcTest.java
@@ -1,19 +1,52 @@
 package com;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class JdbcTest {
 
 	public static void main(String[] args) {
-		
-		
+
+		Connection con = null;
+		//Connection con1 = null;
+
 		try {
+
+			//DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+			//Class.forName("com.mysql.cj.jdbc.Driver");
+			//System.out.println("--- Driver class is loaded...");
+
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root1234");
 			
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			System.out.println("--- Driver class is ,loaded...");
-			
+			//con1 = DriverManager.getConnection("jdbc:mysql://localhost:3306/cts", "productdb", "mypwd");
+
+			System.out.println("-- Connected to " + con.getMetaData().getDatabaseProductName());
+
+			String q1 = "insert into product values('P004','Bottle',555)";
+
+			Statement stmt = con.createStatement();
+
+			int res = stmt.executeUpdate(q1);
+
+			if (res != 0) {
+				System.out.println(res + " Records(s) inserted");
+
+			} else {
+				System.out.println("-- Some issue while saving...");
+			}
+
 		} catch (Exception e) {
-			System.out.println("--- Failed due to "+e);
+			System.out.println("--- Failed due to " + e);
+		} finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
-		
 
 	}
 
