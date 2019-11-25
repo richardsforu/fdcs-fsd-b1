5ba7987757124f78c860c251e20ba3c2aade31ec
praveen.somireddy@gmail.com <praveen.somireddy@gmail.com>
2019-11-25 12:07:16 +0530

    Hibernate

--- Hibernate/Hibernate-2/src/main/java/com/Test2.java
+++ Hibernate/Hibernate-2/src/main/java/com/Test2.java
@@ -1,15 +1,7 @@
 package com;
 
-import java.util.HashMap;
 
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Environment;
-import org.hibernate.service.ServiceRegistry;
 
 import com.cts.product.config.DBConfig;
 
