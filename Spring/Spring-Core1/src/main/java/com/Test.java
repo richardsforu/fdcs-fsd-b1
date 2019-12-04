5466555a3243116d867e4f68f3eda9eebf31e2b2
praveen.somireddy@gmail.com <praveen.somireddy@gmail.com>
2019-12-04 12:22:38 +0530

    Spring Core

---
+++ Spring/Spring-Core1/src/main/java/com/Test.java
@@ -0,0 +1,20 @@
+package com;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		ApplicationContext ac = new ClassPathXmlApplicationContext("spring.xml");
+
+		Employee e1 = ac.getBean("emp", Employee.class);
+		Employee e2 = ac.getBean("emp", Employee.class);
+		
+		e1.testEmployee();
+		e2.testEmployee();
+
+	}
+
+}
